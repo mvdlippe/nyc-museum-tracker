43f0061cb4acc9dea172b1e84feb563aa126aec0
Matt von der Lippe <mvdlippe@gmail.com>
2021-08-11 11:55:17 -0400

    Added some conditional formatting to the react MuseumCard component

--- client/components/MuseumCard.js
+++ client/components/MuseumCard.js
@@ -7,6 +7,33 @@ const MuseumCard = ({
   // Destructure the passed down info
   const { name, tel, url, adress1, address2, city, zip } = info;
 
+  function Address(info) {
+    // If address2 exists, return both adress1 and address2; otherwise just return adress1
+    if (address2) {
+      return (
+        <li>Address:
+          <p>{adress1}</p>
+          <p>{address2}</p>
+        </li>
+      );
+    } else {
+      return (
+        <li>Address: 
+          <p>{adress1}</p>
+        </li>
+      );
+    }
+  }
+
+  function Zip(info) {
+    // If the zipcode exists, return it in a li, otherwise return nothing.
+    const zipInt = parseInt(zip);
+    if (zipInt) {
+      return <li>Zip: {zipInt}</li>;
+    }
+    return null;
+  }
+
   return (
     <article>
       <div>
@@ -14,9 +41,8 @@ const MuseumCard = ({
       </div>
       <ul>
         <li>Borough: {city === 'New York' ? 'Manhattan' : city}</li>
-        <li>Address: {adress1}</li>
-        <li>{address2}</li>
-        <li>{zip}</li>
+        <Address />
+        <Zip />
         <li><a href={url}>{url}</a></li>
         <li>Telephone: {tel}</li>
       </ul>
